4da5a188b874b755bcf923924ec9c7ed4f662e59
ali-tltech <murthasaali.tltechnologies@gmail.com>
2025-04-10 19:25:50 +0530

    Updated SEO OPengraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
+  
+  
 export const metadata = {
   title: "SWAYAMWARA SILKS || EVENT BOOKING",
   description: "Discover the elegance of traditional silk sarees and modern designs at our showroom.",
-  
+  openGraph: {
+    title: 'SWAYAMWARA SILKS || EVENT BOOKING',
+    description: 'Discover the elegance of traditional silk sarees and modern designs at our showroom.',
+    url: 'https://ss-in.vercel.app/',
+    siteName: 'Your Site Name',
+    images: [
+      {
+        url: 'https://opengraph.b-cdn.net/production/images/63ba749b-0ba3-49b6-9517-c3d52358c6f2.png?token=vmcL7cQnaFxrrZ7cb4mcK_C2mOF8TKU3I_lFDGecvK4&height=617&width=1200&expires=33280293296', // place it in public folder
+        width: 1200,
+        height: 630,
+        alt: 'OG Image Alt Text',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
